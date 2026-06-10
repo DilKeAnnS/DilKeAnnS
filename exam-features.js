38a6e256fe8a88f7a3a7d6f91040f16aba87c768
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-06-10 20:27:24 +0100

    Create exam-features.js

---
+++ exam-features.js
@@ -0,0 +1,21 @@
+const examFeatures = [
+
+  "Eligibility Checker",
+  "Age Calculator",
+  "Qualification Checker",
+  "Exam Calendar",
+  "Latest Notifications",
+  "Official Links",
+  "Previous Year Papers",
+  "Mock Tests",
+  "Cut Off Analysis",
+  "Rank Predictor",
+  "AI Career Guidance",
+  "Exam Reminder",
+  "Syllabus Download",
+  "Admit Card Alerts",
+  "Result Alerts",
+  "Counselling Alerts",
+  "Scholarship Finder"
+
+];
