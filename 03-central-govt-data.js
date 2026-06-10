7f8a4caae941a9ce883138dcafb4208ece6bf17f
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-06-10 20:13:31 +0100

    Create 03-central-govt-data.js

---
+++ 03-central-govt-data.js
@@ -0,0 +1,126 @@
+const centralGovtJobs = [
+
+{
+    id: "upsc",
+    name: "UPSC",
+    jobs: [
+        "IAS",
+        "IPS",
+        "IFS",
+        "IRS",
+        "IAAS",
+        "IDAS",
+        "Indian Information Service",
+        "Indian Railway Management Service"
+    ]
+},
+
+{
+    id: "ssc",
+    name: "SSC",
+    jobs: [
+        "CGL",
+        "CHSL",
+        "MTS",
+        "CPO",
+        "JE",
+        "Stenographer",
+        "Selection Post"
+    ]
+},
+
+{
+    id: "railway",
+    name: "Railway",
+    jobs: [
+        "ALP",
+        "Technician",
+        "JE",
+        "SSE",
+        "Station Master",
+        "Goods Guard",
+        "TC",
+        "Clerk"
+    ]
+},
+
+{
+    id: "banking",
+    name: "Banking",
+    jobs: [
+        "IBPS PO",
+        "IBPS Clerk",
+        "SBI PO",
+        "SBI Clerk",
+        "RBI Assistant",
+        "RBI Grade B",
+        "NABARD Officer"
+    ]
+},
+
+{
+    id: "psu",
+    name: "PSU Jobs",
+    jobs: [
+        "BHEL",
+        "NTPC",
+        "ONGC",
+        "IOCL",
+        "GAIL",
+        "SAIL",
+        "NHPC",
+        "PowerGrid"
+    ]
+},
+
+{
+    id: "income_tax",
+    name: "Income Tax Department",
+    jobs: [
+        "Tax Assistant",
+        "Inspector",
+        "Assistant Commissioner"
+    ]
+},
+
+{
+    id: "cbi",
+    name: "CBI",
+    jobs: [
+        "Sub Inspector",
+        "Inspector",
+        "DSP"
+    ]
+},
+
+{
+    id: "nia",
+    name: "NIA",
+    jobs: [
+        "Inspector",
+        "DSP",
+        "Investigation Officer"
+    ]
+},
+
+{
+    id: "epfo",
+    name: "EPFO",
+    jobs: [
+        "SSA",
+        "EO/AO",
+        "Accounts Officer"
+    ]
+},
+
+{
+    id: "esic",
+    name: "ESIC",
+    jobs: [
+        "UDC",
+        "MTS",
+        "Social Security Officer"
+    ]
+}
+
+];
