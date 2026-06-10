2e5da2140bc767775ab8352b9c595ad9deb8c1f6
DilKeAnnS <DilKeAnnsCarrierSathi@gmail.com>
2026-06-10 20:12:04 +0100

    Create 02-defence-data.js

---
+++ 02-defence-data.js
@@ -0,0 +1,346 @@
+const defenceJobs = [
+
+{
+    id: "indian_army",
+    name: "Indian Army",
+    icon: "🪖",
+    totalEntries: "2000+",
+
+    careers: {
+
+        pass8: [
+            "Tradesman",
+            "Support Staff"
+        ],
+
+        pass10: [
+            "Soldier GD",
+            "Military Police",
+            "Driver"
+        ],
+
+        pass12: [
+            "Soldier Technical",
+            "Clerk",
+            "Store Keeper Technical"
+        ],
+
+        diploma: [
+            "Junior Technical Entry",
+            "Technical Supervisor"
+        ],
+
+        graduate: [
+            "NDA Entry",
+            "CDS Entry",
+            "Technical Graduate Course",
+            "Short Service Commission"
+        ],
+
+        postgraduate: [
+            "Education Corps Officer",
+            "Research Officer"
+        ],
+
+        officer: [
+            "Lieutenant",
+            "Captain",
+            "Major",
+            "Lt Colonel",
+            "Colonel",
+            "Brigadier",
+            "Major General",
+            "Lieutenant General",
+            "General"
+        ]
+    }
+},
+
+{
+    id: "indian_navy",
+    name: "Indian Navy",
+    icon: "⚓",
+    totalEntries: "1500+",
+
+    careers: {
+
+        pass10: [
+            "Sailor Entry",
+            "Tradesman"
+        ],
+
+        pass12: [
+            "SSR",
+            "AA",
+            "Technical Sailor"
+        ],
+
+        diploma: [
+            "Naval Technician"
+        ],
+
+        graduate: [
+            "Executive Officer",
+            "Engineering Officer",
+            "Education Officer"
+        ],
+
+        officer: [
+            "Sub Lieutenant",
+            "Lieutenant",
+            "Commander",
+            "Captain",
+            "Commodore",
+            "Rear Admiral",
+            "Vice Admiral",
+            "Admiral"
+        ]
+    }
+},
+
+{
+    id: "indian_air_force",
+    name: "Indian Air Force",
+    icon: "✈️",
+    totalEntries: "1200+",
+
+    careers: {
+
+        pass10: [
+            "Airman Support Staff"
+        ],
+
+        pass12: [
+            "Airman Group X",
+            "Airman Group Y"
+        ],
+
+        diploma: [
+            "Technical Airman"
+        ],
+
+        graduate: [
+            "Flying Officer",
+            "Ground Duty Officer",
+            "Technical Officer"
+        ],
+
+        officer: [
+            "Flying Officer",
+            "Flight Lieutenant",
+            "Squadron Leader",
+            "Wing Commander",
+            "Group Captain",
+            "Air Commodore",
+            "Air Vice Marshal",
+            "Air Marshal",
+            "Air Chief Marshal"
+        ]
+    }
+},
+
+{
+    id: "coast_guard",
+    name: "Indian Coast Guard",
+    icon: "🚢",
+    totalEntries: "500+",
+
+    careers: {
+
+        pass10: [
+            "Navik Domestic Branch"
+        ],
+
+        pass12: [
+            "Navik GD"
+        ],
+
+        diploma: [
+            "Technical Entry"
+        ],
+
+        graduate: [
+            "Assistant Commandant"
+        ]
+    }
+},
+
+{
+    id: "drdo",
+    name: "DRDO",
+    icon: "🚀",
+    totalEntries: "800+",
+
+    careers: {
+
+        iti: [
+            "Technician Apprentice"
+        ],
+
+        diploma: [
+            "Diploma Apprentice"
+        ],
+
+        graduate: [
+            "Scientist B",
+            "Engineer",
+            "Research Associate"
+        ],
+
+        postgraduate: [
+            "Senior Scientist",
+            "Research Fellow"
+        ],
+
+        director: [
+            "Lab Director",
+            "Program Director"
+        ]
+    }
+},
+
+{
+    id: "isro",
+    name: "ISRO",
+    icon: "🛰️",
+    totalEntries: "700+",
+
+    careers: {
+
+        iti: [
+            "Technician B"
+        ],
+
+        diploma: [
+            "Technical Assistant"
+        ],
+
+        graduate: [
+            "Scientist Engineer",
+            "Research Engineer"
+        ],
+
+        postgraduate: [
+            "Senior Scientist"
+        ],
+
+        director: [
+            "Centre Director",
+            "Mission Director"
+        ]
+    }
+},
+
+{
+    id: "bsf",
+    name: "BSF",
+    icon: "🛡️",
+    totalEntries: "900+",
+
+    careers: {
+
+        pass10: [
+            "Constable GD",
+            "Tradesman"
+        ],
+
+        pass12: [
+            "Head Constable"
+        ],
+
+        graduate: [
+            "Assistant Commandant"
+        ]
+    }
+},
+
+{
+    id: "crpf",
+    name: "CRPF",
+    icon: "🛡️",
+    totalEntries: "900+",
+
+    careers: {
+
+        pass10: [
+            "Constable"
+        ],
+
+        pass12: [
+            "Head Constable"
+        ],
+
+        graduate: [
+            "Sub Inspector",
+            "Assistant Commandant"
+        ]
+    }
+},
+
+{
+    id: "cisf",
+    name: "CISF",
+    icon: "🏢",
+    totalEntries: "700+",
+
+    careers: {
+
+        pass10: [
+            "Constable Fireman",
+            "Constable Driver"
+        ],
+
+        pass12: [
+            "Head Constable"
+        ],
+
+        graduate: [
+            "Inspector",
+            "Assistant Commandant"
+        ]
+    }
+},
+
+{
+    id: "itbp",
+    name: "ITBP",
+    icon: "🏔️",
+    totalEntries: "700+",
+
+    careers: {
+
+        pass10: [
+            "Constable GD"
+        ],
+
+        pass12: [
+            "Head Constable"
+        ],
+
+        graduate: [
+            "Sub Inspector",
+            "Assistant Commandant"
+        ]
+    }
+},
+
+{
+    id: "agniveer",
+    name: "Agniveer",
+    icon: "🔥",
+    totalEntries: "300+",
+
+    careers: {
+
+        pass10: [
+            "Agniveer General Duty"
+        ],
+
+        pass12: [
+            "Agniveer Technical",
+            "Agniveer Clerk"
+        ]
+    }
+}
+
+];
